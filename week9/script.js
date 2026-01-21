40c91b2e1df9a3bc222adaf5c21bbc4ba198e567
Clor0phyte <nikitamazur008@gmail.com>
2026-01-21 09:06:28 +0200

    calculator

---
+++ week9/script.js
@@ -0,0 +1,29 @@
+function Calculator() {
+    let a = prompt("Enter first number:");
+    let b = prompt("Enter second number:");
+    let operation = prompt("Enter operation (+, -, *, /):");
+
+    switch (operation){
+        case "+":
+            alert(`Result: ${Number(a) + Number(b)}`);
+            break;
+        case "-":
+            alert(`Result: ${Number(a) - Number(b)}`);
+            break;
+        case "*":
+            alert(`Result: ${Number(a) * Number(b)}`);
+            break;
+        case "/":
+            if (b == 0) {
+                alert("Error: Division by zero");
+            } else {
+                alert(`Result: ${Number(a) / Number(b)}`);
+            }
+            break;
+        default:
+            alert("Error: Invalid operation");
+            break;
+    }
+    };
+
+Calculator();
