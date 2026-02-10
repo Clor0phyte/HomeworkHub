6e8ebdbfae8c18199740a83a88dd6b8c6bbad598
Clor0phyte <nikitamazur008@gmail.com>
2026-02-10 09:29:58 +0200

    fixed math problem

--- week7/budget.js
+++ week7/budget.js
@@ -4,9 +4,10 @@ let food = prompt("enter food spendings:")
 let transport = prompt("enter transport spendings")
 let entertaiment = prompt("enter entertaiment cost")
 
-let TotalExpenses = rent + food + transport + entertaiment
+let TotalExpenses = Number(rent) + Number(food) + Number(transport) + Number(entertaiment)      
 
 let savings = income - TotalExpenses
+console.log(TotalExpenses)
 
 if (savings > 0){
     console.log("У вас залишилися гроші")
